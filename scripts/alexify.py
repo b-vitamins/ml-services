dcacb93400e803e045f10e8014ad95d48c0f0c85
Ayan Das <bvits@riseup.net>
2024-05-12 23:01:23 +0530

    modify `scripts/alexify.py`

--- scripts/alexify.py
+++ scripts/alexify.py
@@ -20,13 +20,14 @@ FUZZY_MATCH_THRESHOLD = 80
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
         logging.FileHandler(os.path.join(LOG_DIR, "alexify.log")),
         logging.StreamHandler(),
     ],
 )
 
-PROCESSED_ENTRIES_LOG = os.path.join(LOG_DIR, "processed_entries.log")
+PROCESSED_ENTRIES_LOG = os.path.join(LOG_DIR, "alexify.processed.log")
 
 
 def load_processed_entries():
