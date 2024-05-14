964d86aa937d04d239d09f53c57e034f4930d0a7
Ayan Das <bvits@riseup.net>
2024-05-14 08:28:35 +0530

    modify `alexify.py`
    - add functions to aggregate all unique `referenced_works` from all OA
    Work objects under `data`.
    - fetch those works and save them in `data/deps`.

--- scripts/alexify.py
+++ scripts/alexify.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import logging
 from fuzzywuzzy import fuzz
 import pyalex
@@ -13,6 +14,7 @@ pyalex.config.retry_http_codes = [429, 500, 503]
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(SCRIPT_DIR, "..", "data")
+DEP_DIR = os.path.join(SCRIPT_DIR, "..", "data", "deps")
 LOG_DIR = os.path.join(SCRIPT_DIR, "..", "logs")
 CONFERENCES = [
     "pmlr",
@@ -28,7 +30,7 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
-        logging.FileHandler(os.path.join(LOG_DIR, "alexify.log")),
+        logging.FileHandler(os.path.join(LOG_DIR, "alexify.deps.log")),
         logging.StreamHandler(),
     ],
 )
@@ -57,7 +59,6 @@ def fuzzy_match_titles(extracted_title, work_title):
         return fuzz.ratio(extracted_title, work_title) > FUZZY_MATCH_THRESHOLD
     else:
         return False
-
 
 def process_search_results(bib_file_path, openalex_folder):
     """Process search results to find matching titles."""
@@ -172,6 +173,56 @@ def main():
             except Exception as e:
                 logging.error(f"Error processing BibTeX file: {e}")
 
+def unique_urls(directory):
+    urls = set()
+    
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".json"):
+                file_path = os.path.join(root, file)
+                try:
+                    with open(file_path, 'r') as f:
+                        data = json.load(f)
+                        referenced_works = data.get("referenced_works", [])
+                        urls.update(referenced_works)
+                except Exception as e:
+                    print(f"Error processing file {file_path}: {e}")
+    return urls
+
+def process_urls(url_list, dep_dir, requests_per_day=100000):
+    processed_count = 0
+    start_time = time.time()
+
+    for i, full_id_link in enumerate(url_list):
+        if processed_count >= requests_per_day:
+            logging.info(f"Reached daily quota of {requests_per_day} requests. Stopping processing.")
+            break
+
+        work_id = full_id_link.rsplit("/", 1)[-1] if full_id_link else None
+        if work_id:
+            file_path = os.path.join(dep_dir, f"{work_id}.json")
+            if os.path.exists(file_path):
+                logging.info(f"File {work_id}.json already exists. Skipping download.")
+                continue
+
+            try:
+                work = pyalex.Works()[work_id]
+                if work:
+                    save_json(work, dep_dir, work_id)
+                    processed_count += 1
+                    logging.info(f"Successfully processed and saved {work_id}.json")
+                else:
+                    logging.warning(f"No work found for ID {work_id}")
+            except Exception as e:
+                logging.error(f"Error fetching work for ID {work_id}: {e}")
+        
+        if (i + 1) % 1000 == 0:
+            elapsed_time = time.time() - start_time
+            logging.info(f"Processed {i + 1}/{len(url_list)} URLs in {elapsed_time//3600}h {elapsed_time%3600//60}m.")
+
 
 if __name__ == "__main__":
-    main()
+    if not os.path.exists(DEP_DIR):
+        os.makedirs(DEP_DIR)
+    url_list = unique_urls(DATA_DIR)
+    process_urls(url_list, DEP_DIR)
