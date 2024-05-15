ad4b7213e0adc233c7ec398444106d60f250ad20
Ayan Das <bvits@riseup.net>
2024-05-15 23:52:51 +0530

    blacken `alexify-deps.py`

--- scripts/alexify-deps.py
+++ scripts/alexify-deps.py
@@ -30,11 +30,13 @@ logging.basicConfig(
     ],
 )
 
+
 def save_json(data, folder, original_filename):
     """Saves data as JSON in the specified folder."""
     filename = f"{original_filename}.json"
     with open(os.path.join(folder, filename), "w") as json_file:
         json.dump(data, json_file)
+
 
 def process_deps(url_list, dep_dir, requests_per_day=100000):
     """Process URLs to fetch data from OpenAlex."""
@@ -42,12 +44,16 @@ def process_deps(url_list, dep_dir, requests_per_day=100000):
     start_time = time.time()
     for i, full_id_link in enumerate(url_list):
         if processed_count >= requests_per_day:
-            logging.info(f"Reached daily quota of {requests_per_day} requests. Stopping processing.")
+            logging.info(
+                f"Reached daily quota of {requests_per_day} requests. Stopping processing."
+            )
             break
         work_id = full_id_link.rsplit("/", 1)[-1]
         file_path = os.path.join(dep_dir, f"{work_id}.json")
         if os.path.exists(file_path):
-            logging.info(f"File {work_id}.json already exists. Skipping download.")
+            logging.info(
+                f"File {work_id}.json already exists. Skipping download."
+            )
             continue
         try:
             work = pyalex.Works()[work_id]
@@ -58,7 +64,10 @@ def process_deps(url_list, dep_dir, requests_per_day=100000):
             logging.error(f"Error fetching work for ID {work_id}: {e}")
         if (i + 1) % 1000 == 0:
             elapsed_time = time.time() - start_time
-            logging.info(f"Processed {i + 1}/{len(url_list)} URLs in {elapsed_time//3600}h {elapsed_time%3600//60}m.")
+            logging.info(
+                f"Processed {i + 1}/{len(url_list)} URLs in {elapsed_time//3600}h {elapsed_time%3600//60}m."
+            )
+
 
 def unique_deps(directory):
     """Collect unique URLs from JSON files in a directory."""
@@ -70,12 +79,13 @@ def unique_deps(directory):
             if file.endswith(".json"):
                 file_path = os.path.join(root, file)
                 try:
-                    with open(file_path, 'r') as f:
+                    with open(file_path, "r") as f:
                         data = json.load(f)
                         urls.update(data.get("referenced_works", []))
                 except Exception as e:
                     logging.error(f"Error processing file {file_path}: {e}")
     return urls
+
 
 if __name__ == "__main__":
     os.makedirs(DEP_DIR, exist_ok=True)
