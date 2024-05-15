fbc02c8113e703389fbf26b02340350ff8acc6fa
Ayan Das <bvits@riseup.net>
2024-05-15 23:53:28 +0530

    blacken `alexify-authors.py`

--- scripts/alexify-authors.py
+++ scripts/alexify-authors.py
@@ -31,20 +31,22 @@ logging.basicConfig(
     ],
 )
 
+
 def save_json(data, folder, original_filename):
     """Saves data as JSON in the specified folder."""
     filename = f"{original_filename}.json"
     with open(os.path.join(folder, filename), "w") as json_file:
         json.dump(data, json_file)
 
+
 def unique_authors(data_dir):
     """
     Scans all JSON files in the specified data directory and its subdirectories,
     extracting and returning a set of unique authors based on their OpenAlex IDs.
-    
+
     Args:
     data_dir (str): The directory to search for JSON files containing author data.
-    
+
     Returns:
     set: A set of unique authors represented by their OpenAlex IDs.
     """
@@ -56,7 +58,7 @@ def unique_authors(data_dir):
             if file.endswith(".json"):
                 file_path = os.path.join(root, file)
                 try:
-                    with open(file_path, 'r') as f:
+                    with open(file_path, "r") as f:
                         data = json.load(f)
                         authorships = data.get("authorships", [])
                         for authorship in authorships:
@@ -65,10 +67,13 @@ def unique_authors(data_dir):
                             if author_id:
                                 unique_authors.add(author_id)
                 except json.JSONDecodeError as e:
-                    logging.error(f"JSON decoding error in file {file_path}: {e}")
+                    logging.error(
+                        f"JSON decoding error in file {file_path}: {e}"
+                    )
                 except Exception as e:
                     logging.error(f"Error processing file {file_path}: {e}")
     return unique_authors
+
 
 def process_authors(url_list, author_dir, requests_per_day=100000):
     """Process URLs to fetch data from OpenAlex."""
@@ -76,12 +81,16 @@ def process_authors(url_list, author_dir, requests_per_day=100000):
     start_time = time.time()
     for i, full_id_link in enumerate(url_list):
         if processed_count >= requests_per_day:
-            logging.info(f"Reached daily quota of {requests_per_day} requests. Stopping processing.")
+            logging.info(
+                f"Reached daily quota of {requests_per_day} requests. Stopping processing."
+            )
             break
         author_id = full_id_link.rsplit("/", 1)[-1]
         file_path = os.path.join(author_dir, f"{author_id}.json")
         if os.path.exists(file_path):
-            logging.info(f"File {author_id}.json already exists. Skipping download.")
+            logging.info(
+                f"File {author_id}.json already exists. Skipping download."
+            )
             continue
         try:
             author = pyalex.Authors()[author_id]
@@ -92,7 +101,10 @@ def process_authors(url_list, author_dir, requests_per_day=100000):
             logging.error(f"Error fetching work for ID {author_id}: {e}")
         if (i + 1) % 1000 == 0:
             elapsed_time = time.time() - start_time
-            logging.info(f"Processed {i + 1}/{len(url_list)} URLs in {elapsed_time//3600}h {elapsed_time%3600//60}m.")
+            logging.info(
+                f"Processed {i + 1}/{len(url_list)} URLs in {elapsed_time//3600}h {elapsed_time%3600//60}m."
+            )
+
 
 if __name__ == "__main__":
     os.makedirs(AUTHORS_DIR, exist_ok=True)
